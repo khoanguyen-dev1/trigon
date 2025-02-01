eed9675c64909603d44ecd7bf8f4803cccbb3993
khoanguyen-dev1 <kn949892@gmail.com>
2025-02-01 17:23:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import re
 
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/trigon', methods=['GET'])
 def trigon():
     url = request.args.get('link')  
@@ -14,30 +15,27 @@ def trigon():
         return jsonify({'error': 'Missing parameter: link'}), 400
 
     try:
-
-        response = requests.get(url)
+        # Setting a timeout of 60 seconds for the initial request
+        response = requests.get(url, timeout=60)
         response.raise_for_status()
 
         soup = BeautifulSoup(response.text, 'html.parser')
         
-
         button = soup.find('div', class_='glass-light')
         
         if not button:
             return jsonify({"error": "Không tìm thấy phần tử với class 'glass-light'."}), 400
         
-
         onclick_attr = button.get('onclick', '')
         
-
         match = re.search(r"window\.location\.href=['\"](https?://[^\s'\";]+)['\"]", onclick_attr)
         
         if match:
             redirect_url = match.group(1)
             
-
+            # Setting a timeout of 60 seconds for the API request
             api_url = f"https://iwoozie.baby/api/free/bypass?url={redirect_url}"
-            api_response = requests.get(api_url)
+            api_response = requests.get(api_url, timeout=60)
 
             if api_response.status_code == 200:
                 api_data = api_response.json()  
@@ -57,4 +55,5 @@ def trigon():
         return jsonify({"error": f"{str(e)}"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=port,debug=False) 
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
